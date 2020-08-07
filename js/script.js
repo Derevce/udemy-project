9ce376bee2bf7790685f4fabad9ac8fc1b8a661d
Natalka <natalkans43@gmail.com>
2020-08-08 00:30:22 +0300

    console app v.1.1

--- js/script.js
+++ js/script.js
@@ -1,20 +1,53 @@
 "use strict" ;// этот код работает в современном режиме
 
-let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
 
-let personalMovieDB = {
+const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
     privat: false
 };
-let i = 0;
-do {
-    i += 1;
-    let lastFilmTitle = prompt("Один из последних просмотренных фильмов?");
-    let lastFilmRate = prompt("На сколько оцените его?");
-    personalMovieDB.movies[lastFilmTitle] = lastFilmRate;
-  } while (i < 2);
+
+for (let i = 0; i < 2; i++ ) {
+  const a = prompt("Один из последних просмотренных фильмов?", ""),
+        b = prompt("На сколько оцените его?", "");
+
+      if (a !=null && b !=null && a != "" && b !="" && a.length < 50 && b.length < 50) {
+         console.log("done");
+         personalMovieDB.movies[a] = b;
+      } else {
+        i--;
+        console.log("error");
+      }
+       
+}
+
+if (personalMovieDB.count < 10) {
+  console.log("Просмотрено довольно мало фильмов");
+} else if (10 <= personalMovieDB.count < 30) {
+  console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+  console.log("Вы киноман");
+} else {
+  console.log("Произошла ошибка");
+}
+
+
+//let i = 0;
+// do { 
+//     i++;
+//     let a = prompt("Один из последних просмотренных фильмов?");
+//     let b = prompt("На сколько оцените его?");
+//     personalMovieDB.movies[a] = b;
+//} while (i < 2);
+
+// let i = 0;
+// while (i < 2) { // выводит 0, затем 1, затем 2
+//   let a = prompt("Один из последних просмотренных фильмов?");
+//   let b = prompt("На сколько оцените его?");
+//   i++;
+// }
 
 console.log(personalMovieDB);
